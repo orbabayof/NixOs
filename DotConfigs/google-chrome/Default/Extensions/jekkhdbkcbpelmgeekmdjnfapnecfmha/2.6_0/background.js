0c5096d06158f5ddd5a498e5e48f0a5db2cde07b
System administrator <orbabayof@gmail.com>
2024-04-13 08:09:24 +0300

    fixing again ... :(

--- DotConfigs/google-chrome/Default/Extensions/jekkhdbkcbpelmgeekmdjnfapnecfmha/2.6_0/background.js
+++
@@ -1,7 +0,0 @@
-//Stores PC in chrome storage which will be used in TE37 ping
-
-chrome.storage.local.set({ "pc": "U501"});
-
-chrome.action.onClicked.addListener(function (tab) {		
-	chrome.tabs.create({url: "chrome://newtab"});
-});
